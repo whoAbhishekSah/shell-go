1cb3d34f9058d1c718bc9dc7fc0604d6facecd49
Abhishek Sah <abhi.sah.97@gmail.com>
2024-11-18 20:08:07 +0530

    process echo command

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -7,8 +7,10 @@ import (
 	"strings"
 )
 
-const ExitCmd = "exit 0"
-
+const (
+	ExitCmd = "exit 0"
+	EchoCmd = "echo"
+)
 func main() {
 	for {
 		// Uncomment this block to pass the first stage
@@ -21,17 +23,43 @@ func main() {
 		if err != nil {
 			panic("error in reading command")
 		}
-		
+
 		if input == ExitCmd {
 			os.Exit(0)
 		}
 
-		if !isValidCmd(input) {
-			fmt.Printf("%s: command not found\n", input)
+		splittedCmd := strings.Split(input, " ")
+		cmd := splittedCmd[0]
+		if !isValidCmd(cmd) {
+			fmt.Printf("%s: command not found\n", cmd)
 		}
+		
+		// skip first item and join the values to get args str
+		args := strings.Join(splittedCmd[1:], " ")
+		processCmd(cmd, args)
+
 	}
 }
 
-func isValidCmd(_ string) bool {
+func isValidCmd(cmd string) bool {
+	validCommands := []string{"echo"}
+	return stringFoundInArray(validCommands, cmd)
+}
+
+func stringFoundInArray(ary []string, stringToSearch string) bool {
+	for _, item := range ary {
+		if item == stringToSearch {
+			return true
+		}
+	}
 	return false
+}
+
+func processCmd(inputCmd string, args string) {
+	switch inputCmd {
+	case EchoCmd:
+		fmt.Println(args)
+	default:
+		return
+	}
 }
