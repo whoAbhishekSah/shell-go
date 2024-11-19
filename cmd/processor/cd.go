f76202907c6af991acd84448bf679dc6d78ae5af
Abhishek Sah <abhi.sah.97@gmail.com>
2024-11-19 17:45:59 +0530

    implement cd to home dir

--- cmd/processor/cd.go
+++ cmd/processor/cd.go
@@ -7,9 +7,19 @@ import (
 	"github.com/codecrafters-io/shell-starter-go/cmd/index"
 )
 
+const (
+	cdHomeDirArg = "~"
+	homeEnvVarKeyName = "HOME"
+)
+
 func ProcessCdCmd(cmd string, args string) {
 	if cmd != index.CdCmd {
 		panic(fmt.Sprintf("wrong command, %s", cmd))
+	}
+
+	if args == cdHomeDirArg {
+		homeDir := os.Getenv(homeEnvVarKeyName)
+		args = homeDir
 	}
 	
 	err := os.Chdir(args)
